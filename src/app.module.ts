4b28f01291aed6d69736343d82048136e21bd387
Miguel Enrique Garcia Pinto <136664358+miguelegp73@users.noreply.github.com>
2026-09-12 17:26:28 -0300

    feat: register M.A.D.I. interaction module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,8 @@
 import { Module } from '@nestjs/common';
+import { InteractionModule } from './application/interaction/interaction.module';
 import { HealthModule } from './core/health/health.module';
 
 @Module({
-  imports: [HealthModule],
+  imports: [HealthModule, InteractionModule],
 })
 export class AppModule {}
