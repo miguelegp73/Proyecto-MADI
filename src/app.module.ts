5dc23d994bdd45e55f09bd491230a58fcfae392a
Miguel Enrique Garcia Pinto <136664358+miguelegp73@users.noreply.github.com>
2026-09-12 21:00:28 -0300

    feat: enable authentication module

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,8 +2,9 @@ import { Module } from '@nestjs/common';
 import { InteractionModule } from './application/interaction/interaction.module';
 import { ToolsModule } from './application/tools/tools.module';
 import { HealthModule } from './core/health/health.module';
+import { AuthenticationModule } from './application/authentication/authentication.module';
 
 @Module({
-  imports: [HealthModule, InteractionModule, ToolsModule],
+  imports: [HealthModule, InteractionModule, ToolsModule, AuthenticationModule],
 })
 export class AppModule {}
