189421f6ff633a32c1189763445d0908e0852a0c
Miguel Enrique Garcia Pinto <136664358+miguelegp73@users.noreply.github.com>
2026-09-13 01:21:20 -0300

    feat: register authorized local application capability

--- src/application/capabilities/default.capabilities.module.ts
+++ src/application/capabilities/default.capabilities.module.ts
@@ -5,29 +5,28 @@ import { MadiStatusCapability } from '../../core/capabilities/madi.status.capabi
 import { MadiTimeCapability } from '../../core/capabilities/madi.time.capability';
 import { MadiCapabilitiesCapability } from '../../core/capabilities/madi.capabilities.capability';
 import { SystemOpenUrlCapability } from '../../core/capabilities/system.open.url.capability';
+import { SystemOpenAppCapability } from '../../core/capabilities/system.open.app.capability';
 
 export const MADI_CAPABILITY_REGISTRY = Symbol('MADI_CAPABILITY_REGISTRY');
 export const MADI_CAPABILITY_EXECUTOR = Symbol('MADI_CAPABILITY_EXECUTOR');
 
 @Module({
-  providers: [
-    {
-      provide: MADI_CAPABILITY_REGISTRY,
-      useFactory: () => {
-        const registry = new MadiCapabilityRegistry();
-        registry.register(new MadiStatusCapability());
-        registry.register(new MadiTimeCapability());
-        registry.register(new MadiCapabilitiesCapability(registry));
-        registry.register(new SystemOpenUrlCapability());
-        return registry;
-      },
+  providers: [{
+    provide: MADI_CAPABILITY_REGISTRY,
+    useFactory: () => {
+      const registry = new MadiCapabilityRegistry();
+      registry.register(new MadiStatusCapability());
+      registry.register(new MadiTimeCapability());
+      registry.register(new MadiCapabilitiesCapability(registry));
+      registry.register(new SystemOpenUrlCapability());
+      registry.register(new SystemOpenAppCapability());
+      return registry;
     },
-    {
-      provide: MADI_CAPABILITY_EXECUTOR,
-      useFactory: (registry: MadiCapabilityRegistry) => new RegistryCapabilityExecutor(registry),
-      inject: [MADI_CAPABILITY_REGISTRY],
-    },
-  ],
+  }, {
+    provide: MADI_CAPABILITY_EXECUTOR,
+    useFactory: (registry: MadiCapabilityRegistry) => new RegistryCapabilityExecutor(registry),
+    inject: [MADI_CAPABILITY_REGISTRY],
+  }],
   exports: [MADI_CAPABILITY_REGISTRY, MADI_CAPABILITY_EXECUTOR],
 })
 export class DefaultCapabilitiesModule {}
